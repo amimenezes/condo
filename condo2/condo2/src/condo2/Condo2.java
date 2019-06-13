d2fff1def2566fa6b3201b6d9a43ddbfa71fbd34
mml88 <mi.menezes.luiz@gmail.com>
2019-06-13 11:18:51 -0300

    Add files via upload
    Teste de commit com criação de classes.

---
+++ condo2/condo2/src/condo2/Condo2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package condo2;
+
+/**
+ *
+ * @author 0044172
+ */
+public class Condo2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ condo2/condo2/src/condo2/pessoa.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package condo2;
+
+/**
+ *
+ * @author 0044172
+ */
+public class pessoa {
+    private String nome;
+    private String cpf;
+    private String rg;
+    private String dataNasc;
+
+    public void setNome(String nome){
+      this.nome = nome;
+    }
+
+    public String getNome(){
+      return this.nome;
+    }
+}
